9a5eb18b40d7194145c054ff7385421c1ae3fd91
Ding <705220170@qq.com>
2022-01-15 20:06:29 +0800

    JS

--- ding-live2d-widget/autoload.js
+++ ding-live2d-widget/autoload.js
@@ -1,5 +1,5 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://unpkg.zhimg.com/ding-live2d-widget@1.0.2/";
+const live2d_path = "https://unpkg.zhimg.com/ding-live2d-widget@1.0.3/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
@@ -27,9 +27,9 @@ function loadExternalResource(url, type) {
 // 加载 waifu.css live2d.js waifu-tips.js
 if (screen.width >=1024) {
 	Promise.all([
-		loadExternalResource(live2d_path + "waifu.css", "css"),
-		loadExternalResource(live2d_path + "live2d.js", "js"),
-		loadExternalResource(live2d_path + "waifu-tips.js", "js")
+		loadExternalResource(live2d_path + "waifu.min.css", "css"),
+		loadExternalResource(live2d_path + "live2d.min.js", "js"),
+		loadExternalResource(live2d_path + "waifu-tips.min.js", "js")
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
